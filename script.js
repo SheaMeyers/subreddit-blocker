4df30c4fdc3bf297b8deae3156c21bc74e2bb5db
Shea Meyers <shea.meyers@gmail.com>
2022-12-18 15:29:43 +0100

    Delete full card element

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ async function execute() {
         console.log('Trying to hide')
         var els = document.querySelectorAll("a[href='/r/aww/']")
         console.log('Got els ' + els.length)
-        els.forEach(el => el.remove())
+        els.forEach(el => el.parentElement.parentElement.parentElement.parentElement.remove())
     }, 2000)
 }
 execute();
