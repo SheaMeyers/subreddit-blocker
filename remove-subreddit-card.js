fee89c27d2f07aa12adc7121fb71465e3f452dc5
Shea Meyers <shea.meyers@gmail.com>
2022-12-18 21:38:03 +0100

    Ensure newly added subreddits will be removed

--- remove-subreddit-card.js
+++ remove-subreddit-card.js
@@ -1,11 +1,24 @@
 const execute = () => {
-    setTimeout(() => {
-        chrome.storage.local.get("subreddits", (result) => {
-            result.subreddits.split(',').forEach(subreddit => {
-                var els = document.querySelectorAll(`a[href='/r/${subreddit}/']`)
-                els.forEach(el => el.parentElement.parentElement.parentElement.parentElement.remove())
-            })
-        })
-    }, 5000)
-}
-execute();
+  chrome.storage.local.get("subreddits", (result) => {
+    result.subreddits.split(",").forEach((subreddit) => {
+      var els = document.querySelectorAll(`a[href='/r/${subreddit}/']`);
+      els.forEach((el) =>
+        el.parentElement.parentElement.parentElement.parentElement.remove()
+      );
+    });
+  });
+};
+
+const debounce = (func, timeout = 500) => {
+  let timer;
+  return (...args) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      func.apply(this, args);
+    }, timeout);
+  };
+};
+
+const onNodeInsert = debounce(() => execute());
+
+document.addEventListener("DOMNodeInsertedIntoDocument", onNodeInsert, true);
