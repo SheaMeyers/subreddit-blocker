d6c51f52a1c0ef376ff091ef7361c07be2f5ace3
Shea Meyers <shea.meyers@gmail.com>
2022-12-18 16:40:10 +0100

    Get working with list

--- update-subreddit-list.js
+++ update-subreddit-list.js
@@ -1,7 +1,12 @@
 const setCurrentSubredditList = () => {
     chrome.storage.local.get("subreddits", (result) => {
-        document.getElementById("subreddits-list").innerHTML = result.subreddits
         document.getElementById("subreddit-input").value = ""
+        var ul = document.getElementById("subreddits-list")
+        result.subreddits.split(',').forEach(subreddit => {
+            let li = document.createElement("li")
+            li.innerHTML = subreddit
+            ul.appendChild(li)
+        })
     });
 }
 
