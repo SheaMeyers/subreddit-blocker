8fcaf62d0edf703ba69179dcfdc5395ed553338f
Shea Meyers <shea.meyers@gmail.com>
2022-12-18 15:24:14 +0100

    Get hiding element working

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 async function execute() {
-    console.log('Trying to hide news')
-    var els = document.querySelectorAll("a[href='/r/news']")
-    els.forEach(el => el.remove())
+    setTimeout(() => {
+        console.log('Trying to hide')
+        var els = document.querySelectorAll("a[href='/r/aww/']")
+        console.log('Got els ' + els.length)
+        els.forEach(el => el.remove())
+    }, 2000)
 }
 execute();
