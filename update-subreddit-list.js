a1db0ec6176bd08a529077ec20411ed7d5dd8a84
Shea Meyers <shea.meyers@gmail.com>
2022-12-18 17:27:51 +0100

    Allow removing from list

--- update-subreddit-list.js
+++ update-subreddit-list.js
@@ -1,13 +1,35 @@
+const removeSubredditFromList = (subreddit) => {
+    chrome.storage.local.get("subreddits", (result) => {
+        let subRedditsArray = result.subreddits.split(',')
+        subRedditsArray = subRedditsArray.filter(s => s !== subreddit)
+        chrome.storage.local.set({ "subreddits": subRedditsArray.join(',')  }, () =>  setCurrentSubredditList());
+    }); 
+}
+
+const setSubredditList = (subreddits) => {
+    var ul = document.getElementById("subreddits-list")
+    if (subreddits) {
+        ul.innerHTML = ""
+        subreddits.split(',').forEach(subreddit => {
+            let li = document.createElement("li")
+            li.innerHTML = subreddit
+            let button = document.createElement("button")
+            button.innerHTML = "X"
+            //button.onclick = removeSubredditFromList(subreddit)
+            button.onclick = function() {
+                removeSubredditFromList(subreddit)
+            }
+            li.appendChild(button)
+            ul.appendChild(li)
+        })
+    }
+}
+
 const setCurrentSubredditList = () => {
     chrome.storage.local.get("subreddits", (result) => {
         document.getElementById("subreddit-input").value = ""
-        var ul = document.getElementById("subreddits-list")
-        result.subreddits.split(',').forEach(subreddit => {
-            let li = document.createElement("li")
-            li.innerHTML = subreddit
-            ul.appendChild(li)
-        })
-    });
+        setSubredditList(result.subreddits)
+    })
 }
 
 const addToList = () => {
