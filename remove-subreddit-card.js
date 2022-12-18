eed8bf9764d0eb2c1dfbc977ba026afaaafb77e5
Shea Meyers <shea.meyers@gmail.com>
2022-12-18 17:42:58 +0100

    Use actual list

--- remove-subreddit-card.js
+++ remove-subreddit-card.js
@@ -1,9 +1,11 @@
 const execute = () => {
     setTimeout(() => {
-        console.log('Trying to hide')
-        var els = document.querySelectorAll("a[href='/r/aww/']")
-        console.log('Got els ' + els.length)
-        els.forEach(el => el.parentElement.parentElement.parentElement.parentElement.remove())
-    }, 2000)
+        chrome.storage.local.get("subreddits", (result) => {
+            result.subreddits.split(',').forEach(subreddit => {
+                var els = document.querySelectorAll(`a[href='/r/${subreddit}/']`)
+                els.forEach(el => el.parentElement.parentElement.parentElement.parentElement.remove())
+            })
+        })
+    }, 5000)
 }
 execute();
